af412637018e53b3d961388a6bb723f5d8fd2a93
Olivia Sztanga <oliviasztanga@gmail.com>
2020-12-10 18:40:18 -0500

    Update seed.py

--- seed.py
+++ seed.py
@@ -1,6 +1,31 @@
 # import your models
 
 def seed(session):
-    # write seed function here
+    persons = [
+        Person(name="Laura Jean"),
+        Person(name="Bilbo Baggins"),
+        Person(name="Frodo"),
+    ]
+
+    projects = [
+        Project(name="Term Paper"),
+        Project(name="Destroy the ring"),
+    ]
+
+    tasks = [
+        Task(
+            name="Write term paper",
+            description="Just do it",
+            due_date=datetime.date(2021,10,10),
+            person_id=1,
+            project_id=1,
+        ),
+    ]
+
+    session.add_all(persons)
+    session.add_all(projects)
+    session.add_all(tasks)
+
+    session.commit()
 
     session.commit()
